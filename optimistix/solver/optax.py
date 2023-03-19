f2390a7d2241a904b2ab3816db261c20c37ed4e8
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2023-03-19 16:07:47 -0700

    Added optax as a minimiser

---
+++ optimistix/solver/optax.py
@@ -0,0 +1,55 @@
+from typing import Any, Dict, Tuple
+
+import equinox as eqx
+import jax.numpy as jnp
+from typing_extensions import TypeAlias
+
+from ..minimise import AbstractMinimiser
+
+
+OptaxClass: TypeAlias = Any
+
+
+class OptaxMinimiser(AbstractMinimiser):
+    optax_cls: OptaxClass
+    args: Tuple[Any, ...]
+    kwargs: Dict[str, Any]
+    max_steps: int
+
+    def __init__(self, optax_cls, *args, max_steps, **kwargs):
+        self.optax_cls = optax_cls
+        self.args = args
+        self.kwargs = kwargs
+        self.max_steps = max_steps
+
+    def init(self, problem, y, args, options):
+        del problem, args, options
+        step_index = jnp.array(0)
+        optim = self.optax_cls(*self.args, **self.kwargs)
+        opt_state = optim.init(y)
+        return step_index, opt_state
+
+    def step(self, problem, y, args, options, state):
+        del options
+
+        @eqx.filter_grad
+        def compute_grads(_y):
+            value = problem.fn(_y, args)
+            if problem.has_aux:
+                value, aux = value
+            else:
+                aux = None
+            return value, aux
+
+        grads, aux = compute_grads(y)
+        step_index, opt_state = state
+        optim = self.optax_cls(*self.args, **self.kwargs)
+        updates, new_opt_state = optim.update(grads, opt_state)
+        new_y = eqx.apply_updates(y, updates)
+        new_state = (step_index + 1, new_opt_state)
+        return new_y, new_state, aux
+
+    def terminate(self, problem, y, args, options, state):
+        del problem, y, args, options
+        step_index, _ = state
+        return step_index > self.max_steps
