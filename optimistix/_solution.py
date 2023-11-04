e43a265f45f045b2b0623f46e421ef18d6a84f53
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2023-11-03 19:31:45 -0700

    Compat with Lineax successful message chnage

--- optimistix/_solution.py
+++ optimistix/_solution.py
@@ -23,7 +23,7 @@ from ._custom_types import Aux, Y
 
 # Extend `lineax.RESULTS` as we want to be able to use their error messages too.
 class RESULTS(lx.RESULTS):  # pyright: ignore
-    successful = ""
+    successful = "Success."
     nonlinear_max_steps_reached = (
         "The maximum number of steps was reached in the nonlinear solver. "
         "The problem may not be solveable (e.g., a root-find on a function that has no "
