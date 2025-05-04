56d482a62c451e48b6c7c0533dfb4e18a657b2ec
Johanna Haffner <johanna.haffner@bsse.ethz.ch>
2025-05-04 19:53:09 +0200

    add HATFLDH problem to benchmark collection

--- benchmarks/cutest/__init__.py
+++ benchmarks/cutest/__init__.py
@@ -1,5 +1,6 @@
 from .bt import BT1 as BT1, BT2 as BT2, BT4 as BT4, BT5 as BT5
 from .flt import FLT as FLT
+from .hatfldh import HATFLDH as HATFLDH
 from .problem import (
     AbstractBoundedMinimisation as AbstractBoundedMinimisation,
     AbstractConstrainedMinimisation as AbstractConstrainedMinimisation,
@@ -14,5 +15,6 @@ problems = (
     *(BT4(i) for i in BT4().provided_y0s),
     *(BT5(i) for i in BT5().provided_y0s),
     FLT(),
+    HATFLDH(),
     ROSENBR(),
 )
---
+++ benchmarks/cutest/hatfldh.py
@@ -0,0 +1,70 @@
+import jax.numpy as jnp
+
+from .problem import AbstractConstrainedMinimisation
+
+
+class HATFLDH(AbstractConstrainedMinimisation, strict=True):
+    """The HATFLDH problem from the CUTEST collection of benchmark problems.
+
+    A test problem from the OPTIMA user manual.
+    This is a nonlinear objective with linear constraints.
+
+    Source: "The OPTIMA user manual (issue No.8, p. 91)",
+    Numerical Optimization Centre, Hatfield Polytechnic (UK), 1989.
+
+    SIF input: Ph. Toint, May 1990.
+    Classification: QLR2-AN-4-7
+    """
+
+    def objective(self, y, args):
+        del args
+        x1, x2, x3, x4 = y
+        return -x1 * x3 - x2 * x4
+
+    def y0(self):
+        return jnp.array([1.0, 5.0, 5.0, 1.0])  # This starting point is infeasible
+
+    def args(self):
+        return None
+
+    def constraint(self, y):
+        """The sums of all pairwise combinations of the variables must fall within a
+        specified range of values. The feasible region is four-dimensional diamond,
+        cut in half by the hyperplane specified through the sum constraint
+        x1 + x2 + x3 + x4 >= 5.0.
+        """
+        A = jnp.array(
+            [
+                [1.0, 1.0, 0.0, 0.0],
+                [1.0, 0.0, 1.0, 0.0],
+                [1.0, 0.0, 0.0, 1.0],
+                [0.0, 1.0, 1.0, 0.0],
+                [0.0, 1.0, 0.0, 1.0],
+                [0.0, 0.0, 1.0, 1.0],
+            ]
+        )
+        pairwise_sums = jnp.matmul(A, y)
+
+        constants = jnp.array([2.5, 2.5, 2.5, 2.0, 2.0, 1.5])
+        ranges = jnp.array([5.0, 5.0, 5.0, 5.0, 5.0, 5.0])
+        lower_bounds = constants - 0.5 * ranges
+        upper_bounds = constants + 0.5 * ranges
+
+        distance_from_lower = pairwise_sums - lower_bounds
+        distance_from_upper = upper_bounds - pairwise_sums
+
+        sum_above = jnp.sum(y) - 5.0  # Verified against CUTEST.jl (J. Haffner)
+
+        inequalities = (distance_from_lower, distance_from_upper, sum_above)
+        return None, inequalities
+
+    def bounds(self):
+        lower = jnp.zeros(4)  # Verified against CUTEST.jl (J. Haffner)
+        upper = jnp.full(4, 5.0)
+        return lower, upper
+
+    def expected_result(self):
+        return None
+
+    def expected_objective_value(self):
+        return jnp.array(-24.4999998)
