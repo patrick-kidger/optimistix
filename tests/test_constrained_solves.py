1d9e029a80b38ab44afaacec503a1e3b3f309372
Johanna Haffner <johanna.haffner@bsse.ethz.ch>
2025-05-11 12:13:08 +0200

    remove old tests for clipped BFGS - we do not support this anymore

--- tests/test_constrained_solves.py
+++ tests/test_constrained_solves.py
@@ -171,14 +171,9 @@ def test_slsqp_cauchy_newton(fn, y0, args, constraint, expected_result):
     assert tree_allclose(sol.value, res, rtol=1e-2, atol=1e-2)
 
 
-@pytest.mark.skip(
-    reason="New BFGS (inheriting from AbstractQuasiNewton) does not support clipping."
-)
-@pytest.mark.parametrize(
-    "solver", gauss_newton_optimisers + [optx.BFGS(rtol=1e-04, atol=1e-08)]
-)
+@pytest.mark.parametrize("solver", gauss_newton_optimisers)
 @pytest.mark.parametrize("fn, y0, args, bounds", least_squares_bounded_many_minima)
-def test_gauss_newton_bfgs_clip(solver, fn, y0, args, bounds):
+def test_gauss_newton_clip(solver, fn, y0, args, bounds):
     # Check that the solution is within bounds for functions with minima outside bounds
     sol = optx.least_squares(
         fn,
@@ -193,38 +188,6 @@ def test_gauss_newton_bfgs_clip(solver, fn, y0, args, bounds):
         lambda x, l, u: jnp.logical_and(l <= x, x <= u), sol, lower, upper
     )
     assert jnp.all(jtu.tree_reduce(jnp.logical_and, check_bounds))
-
-
-# TODO: reverse the boundary map: we're only supporting clipping here
-# TODO: do something about the solution.result?
-@pytest.mark.skip(
-    reason=(
-        "BFGS now inherits from AbstractQuasiNewton and does not support clipping. "
-        "(This feature can be resurrected.)"
-    )
-)
-@pytest.mark.parametrize(
-    "fn, y0, args, bounds, expected_result", minimise_bounded_with_local_minima
-)
-def test_bfgs_clip(fn, y0, args, bounds, expected_result):
-    boundary_map = optx.BoxProjection()
-    sol = optx.minimise(
-        fn,
-        optx.BFGS(rtol=1e-03, atol=1e-06),
-        y0,
-        args=args,
-        bounds=bounds,
-        options={"boundary_map": boundary_map},
-    )
-
-    lower, upper = bounds
-    check_bounds = jtu.tree_map(
-        lambda x, l, u: jnp.logical_and(l <= x, x <= u), sol.value, lower, upper
-    )
-    assert jnp.all(jtu.tree_reduce(jnp.logical_and, check_bounds))
-
-    res = jtu.tree_map(lambda x: jnp.asarray(x, dtype=jnp.float64), expected_result)
-    assert tree_allclose(sol.value, res, rtol=1e-3, atol=1e-6)
 
 
 # TODO: This should be the same test as the clipping test above, use parametrize to
