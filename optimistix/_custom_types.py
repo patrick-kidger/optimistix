d246880d14fe39fd97adb43dc980c7cd6b167f39
Johanna Haffner <johanna.haffner@bsse.ethz.ch>
2025-05-11 11:40:15 +0200

    iterates now live in _search

--- optimistix/_custom_types.py
+++ optimistix/_custom_types.py
@@ -14,11 +14,6 @@ SearchState = TypeVar("SearchState")
 DescentState = TypeVar("DescentState")
 Y = TypeVar("Y")
 
-# TODO: cleaner way to represent iterates here. They can be either Y, or a tuple of
-# primal and dual variables. Here I am using a Union as a low-effort way to opening the
-# door to integrating this in a more principled way across optx.
-_Iterate = Union[Y, tuple[Y, Any]]
-
 Constraint: TypeAlias = Callable[[Y], tuple[EqualityOut, InequalityOut]]
 Fn: TypeAlias = Callable[[Y, Args], tuple[Out, Aux]]
 NoAuxFn: TypeAlias = Callable[[Y, Args], Out]
