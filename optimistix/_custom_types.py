a6b5bc1f29843647cb0890d3ae715b2b6333c511
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-05-12 17:33:41 -0400

    linted

--- optimistix/_custom_types.py
+++ optimistix/_custom_types.py
@@ -10,6 +10,7 @@ Out = TypeVar("Out")
 SolverState = TypeVar("SolverState")
 SearchState = TypeVar("SearchState")
 DescentState = TypeVar("DescentState")
+HessianUpdateState = TypeVar("HessianUpdateState")
 Y = TypeVar("Y")
 
 Fn: TypeAlias = Callable[[Y, Args], tuple[Out, Aux]]
