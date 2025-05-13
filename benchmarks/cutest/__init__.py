dab485c56807ad6992dfc4b33f6a6bb9a0199bc7
Johanna Haffner <johanna.haffner@bsse.ethz.ch>
2025-05-13 22:49:50 +0200

    add the curly problem

--- benchmarks/cutest/__init__.py
+++ benchmarks/cutest/__init__.py
@@ -1,4 +1,5 @@
 from .arwhead import ARWHEAD as ARWHEAD
+from .curly import CURLY10 as CURLY10, CURLY20 as CURLY20, CURLY30 as CURLY30
 from .fletch import FLETCBV3 as FLETCBV3
 from .himmelblau import HIMMELBG as HIMMELBG
 from .problem import (
@@ -12,6 +13,15 @@ problems = (
     ARWHEAD(n=500),
     ARWHEAD(n=1000),
     ARWHEAD(n=5000),
+    CURLY10(n=100),
+    CURLY10(n=1000),
+    CURLY10(n=10000),
+    CURLY20(n=100),
+    CURLY20(n=1000),
+    CURLY20(n=10000),
+    CURLY30(n=100),
+    CURLY30(n=1000),
+    CURLY30(n=10000),
     # Not varying the scale term in the FLETCBV3 problem
     FLETCBV3(n=10, extra_term=1),
     FLETCBV3(n=100, extra_term=1),
