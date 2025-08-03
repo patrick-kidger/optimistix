03e47dc1ba9684fc5a4bd9118b4d7c9fe349584d
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-08-03 10:45:36 +0200

    Add L-BFGS solver, which creates a new Hessian update strategy for the quasi-Newton class of solvers. Supports both inverse Hessian and Hessian approximations, and is on par with optax in performance on benchmarks.

--- optimistix/_solver/__init__.py
+++ optimistix/_solver/__init__.py
@@ -25,6 +25,7 @@ from .levenberg_marquardt import (
     IndirectLevenbergMarquardt as IndirectLevenbergMarquardt,
     LevenbergMarquardt as LevenbergMarquardt,
 )
+from .limited_memory_bfgs import LBFGS as LBFGS
 from .nelder_mead import NelderMead as NelderMead
 from .newton_chord import Chord as Chord, Newton as Newton
 from .nonlinear_cg import (
