b09d5c87ba4776606127dbf1a93a6a05bdc0a255
Johanna Haffner <johanna.haffner@bsse.ethz.ch>
2025-10-05 22:07:29 +0200

    remove strict dtype promotion rules - benchmarks are not tests, so we don't need them here. We would otherwise have to use context management for any comparison to Optax minimisers.

--- benchmarks/conftest.py
+++ benchmarks/conftest.py
@@ -7,8 +7,6 @@ import pytest
 
 
 jax.config.update("jax_enable_x64", True)
-jax.config.update("jax_numpy_rank_promotion", "raise")
-# jax.config.update("jax_numpy_dtype_promotion", "strict")
 
 
 @pytest.fixture
