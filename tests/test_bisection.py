3a42f073fa30e8d04ace3407e314a2784b09af79
Gabriel Gerlero <gerlero@users.noreply.github.com>
2026-04-13 13:20:46 -0300

    Add NewtonBisection test case

--- tests/test_bisection.py
+++ tests/test_bisection.py
@@ -57,3 +57,43 @@ def test_bisection(_fn, init, bisection_options, args):
         f_val = out
     zeros = jtu.tree_map(jnp.zeros_like, f_val)
     assert tree_allclose(f_val, zeros, atol=atol, rtol=rtol)
+
+
+@pytest.mark.parametrize(
+    "_fn, init, bisection_options, args", bisection_fn_init_options_args
+)
+def test_newton_bisection(_fn, init, bisection_options, args):
+    solver = optx.NewtonBisection(rtol=1e-6, atol=1e-6)
+    atol = rtol = 1e-4
+    has_aux = random.choice([True, False])
+
+    def root_find_problem(y, args):
+        f_val = _fn(y, args)
+        return (f_val**ω - y**ω).ω
+
+    if _fn == trivial:
+        bisection_problem = _fn
+    else:
+        bisection_problem = root_find_problem
+    if has_aux:
+        fn = lambda x, args: (bisection_problem(x, args), smoke_aux)
+    else:
+        fn = bisection_problem
+
+    optx_fp = optx.root_find(
+        fn,
+        solver,
+        init,
+        has_aux=has_aux,
+        args=args,
+        options=bisection_options,
+        max_steps=10_000,
+        throw=False,
+    ).value
+    out = fn(optx_fp, args)
+    if has_aux:
+        f_val, _ = out
+    else:
+        f_val = out
+    zeros = jtu.tree_map(jnp.zeros_like, f_val)
+    assert tree_allclose(f_val, zeros, atol=atol, rtol=rtol)
